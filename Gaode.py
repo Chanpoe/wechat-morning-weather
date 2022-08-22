99eb92d6e6fb4d347d54311c52f3ce1a1a4a14a3
Chanpoe <84487466+Chanpoe@users.noreply.github.com>
2022-08-22 21:54:27 +0800

    Update Gaode.py

--- Gaode.py
+++ Gaode.py
@@ -1,21 +1,15 @@
 import requests
 
 # 高德天气查询平台 https://lbs.amap.com/api/webservice/guide/api/weatherinfo
+# 选择Web服务获取key 教程：https://blog.csdn.net/gz2580/article/details/111088190
 
-key = '633d4682a142fc65959f68248abc12b7'
+key = 'xxxxxxxxxxxxxxxxxxxxxxx'
 # adcode 城市编码表下载：https://lbs.amap.com/api/webservice/download
 # 江夏区 420115
 # 大同城区 140213
-city_code = '140213'
+city_code = 'xxxxxx'
 
 weather_url = f'https://restapi.amap.com/v3/weather/weatherInfo?key={key}&city={city_code}'
-
-# weather_response = requests.get(weather_url)
-# {"status": "1", "count": "1", "info": "OK", "infocode": "10000", "lives": [
-#     {"province": "山西", "city": "平城区", "adcode": "140213", "weather": "晴", "temperature": "24", "winddirection": "北",
-#      "windpower": "5", "humidity": "36", "reporttime": "2022-08-22 16:10:05"}]}
-
-# print(weather_response.text)
 
 def getWeather():
     weather_response = requests.get(weather_url)
